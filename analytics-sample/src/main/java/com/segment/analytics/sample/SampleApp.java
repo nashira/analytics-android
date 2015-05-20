87f46f4c5cbcd6994ac9ab94e1a89b88dee38a35
Prateek Srivastava <f2prateek@gmail.com>
2015-05-20 14:26:21 -0700

    Stress Test

--- analytics-sample/src/main/java/com/segment/analytics/sample/SampleApp.java
+++ analytics-sample/src/main/java/com/segment/analytics/sample/SampleApp.java
@@ -2,6 +2,7 @@ package com.segment.analytics.sample;
 
 import android.app.Application;
 import com.segment.analytics.Analytics;
+import java.util.concurrent.TimeUnit;
 
 public class SampleApp extends Application {
 
@@ -16,10 +17,20 @@ public class SampleApp extends Application {
       builder.logLevel(Analytics.LogLevel.BASIC);
     }
 
+    builder.flushInterval(5, TimeUnit.MINUTES).flushQueueSize(2000);
+
     // Set the initialized instance as a globally accessible instance.
     Analytics.setSingletonInstance(builder.build());
 
     // Now anytime you call Analytics.with, the custom instance will be returned.
     Analytics.with(this).track("App Launched");
+
+    // Avoid hitting the fLush Size
+    for (int i = 0; i <= 1100; i++) {
+      Analytics.with(this).track("FJK QueueFileStressTest -testQueueFileUnderStress_track");
+      Analytics.with(this).screen("FJK QueueFileStressTest -testQueueFileUnderStress_screen", null);
+    }
+
+    Analytics.with(this).flush();
   }
 }
